d0e411caca9898636734b90c31bd2c1bb159bc8a
Jon Cooper <joncoop1972@gmail.com>
2017-10-23 09:05:15 -0400

    Initial sharing of project

---
+++ MixedLoopPractice.java
@@ -0,0 +1,72 @@
+/*
+ * APCS-A - Mixed Loop Practice
+ *
+ * This program is to give you practice writing methods that use for loops. Your
+ * task is to study the code and output for methods that I have completed. Then 
+ * you should complete and test the methods that are incomplete or missing. 
+ *
+ *  1) oneToThirtyF    - uses a for loop to print the set {1, 2, 3, ... , 30}.
+ *
+ *  2) oneToThirtyW    - uses a while loop to print the set {1, 2, 3, ... , 30}.
+ *
+ *  3) twentyToZeroF   - uses a for loop to print the set {20, 18, 16, ... , 0}.
+ *
+ *  4) twentyToZeroW   - uses a while loop to print the set {20, 18, 16, ... , 0}.
+ *
+ *  5) sumN            - uses a while loop to find the sum of the first n integers. 
+ *
+ *  6) countMtoN       - uses a while loop to print the set {m, m+1, m+2, ... , n}.
+ *
+ *  7) iLoveJava       - uses a while loop to print the sentence "I love java." n
+ *                       times.
+ *
+ *  8) divBy2and9      - uses a for loop to count from 1 to 100 by ones, but only
+ *                       values that are evenly divisible by 2 and 9 are printed.
+ *                       (you'll need to use an if statement.)
+ *
+ *  9) divBy3and8      - uses a while loop to count from 1 to 100 by ones, but only
+ *                       values that are evenly divisible by 3 and 8 are printed.
+ *                       (you'll need to use an if statement.)
+ *
+ * 10) divBy5or7Sum    - uses a while loop to find the sum of all values in the
+ *     		         set {1, 2, 3, ... , 10000} that are divisible by 5 or 7.
+ *
+ * 11) bizzBuzz        - uses a while loop to count from 1 to n by ones. However,
+ *                       if a number is divisible by 3, the word "bizz" should be 
+ *   	 		 printed, and if the number is divisible by 5, "buzz" should
+ * 			 be printed. Sample output:
+ *
+ *			 1, 2, Bizz, 4, Buzz, Bizz, 7, 8, Bizz, Buzz, 11, Bizz, 13, 14, 
+ *                       Bizz Buzz, 16, 17, Bizz, 19, Buzz, Bizz, 22, 23, Bizz, ...
+ *
+ *
+ *  ------------------------------------------------------------------------------
+ *
+ * @author  your name 
+ * @version date
+ */
+ 
+ public class MixedLoopPractice
+ {
+ 	public static void oneToThirtyF()
+ 	{
+ 		
+ 	}
+ 	
+ 	public static void oneToThirtyW()
+ 	{
+ 		
+ 	}
+ 	    
+	// keep making methods here
+    
+    
+ 	public static void main(String[] args)
+ 	{
+ 		oneToThirtyF();
+ 		oneToThirtyW();
+ 		
+ 		// test your methods here
+ 	}
+ }
+ 
